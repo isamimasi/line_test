3a05b4ff38625967ff727d61516880b0cf7493c8
isamimasi <41987332+isamimasi@users.noreply.github.com>
2020-03-25 16:45:03 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import os
 import sys
 import tempfile
 from argparse import ArgumentParser
-from flask import Flask, request, abort, send_from_directory
+from flask import Flask, request, abort, send_from_directory,render_template,make_response
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from linebot import (
@@ -666,6 +666,79 @@ def handle_member_left(event):
 @app.route('/static/<path:path>')
 def send_static_content(path):
     return send_from_directory('static', path)
+@app.route('/')
+def main():
+    Dict={}
+    Dict["message1"]="Python FLASKのテンプレート"
+    Dict["message2"]=["FLASKでサイトをつくるとapp.pyの行数が多くなりがちです。","そこでgetを利用し、HTMLを表示することにしました。"]
+    return render_template("index.html",Dict=Dict)
+#
+#
+#getを利用し、他ページへ遷移します
+#Using "GET" method, the page will be navigated
+@app.route('/trGet', methods=['GET'])
+def transit_get():
+    Dict={}
+    import appTranstionPathwayGet
+    if request.args.get("pathID"):
+        print (request.args.get)
+        Dict,html=appTranstionPathwayGet.pathwayFromGet(request.args)
+    else:
+        html="message.html"
+        Dict["message1"]="Error1"
+        Dict["message2"]="Error2"
+    return render_template(html,Dict=Dict)
+#
+#
+#Using "POST" method, the page will be shown
+@app.route('/trPost', methods=['POST'])
+def transit_post():
+    if request.method == 'POST' :
+        import appTranstionPathwayPost
+        Dict={}
+        for i in request.form.keys():
+            Dict[i]=request.form[i]
+        html,Dict=appTranstionPathwayPost.pathwayFromPost(Dict)
+        ####if data contains cookie, let them eat
+        #### クッキーがあったらいただきます。
+        if "cookieName" in Dict.keys():
+            print ("cookie done")
+            responsePage=make_response(render_template(html,Dict = Dict))
+            responsePage.set_cookie(Dict["cookieName"], json.dumps(Dict["cookieData"]))
+            return responsePage
+    else:
+        print ("直接アクセス")
+        Dict["message1"]="エラー"
+        Dict["message2"]=["こちらは直接アクセスできません","ホームページからアクセスしてください"]
+        html='message.html'
+    return render_template(html,Dict=Dict)
+###########################################################
+#Error Transaction
+#
+#
+@app.route('/403')
+def abort403():
+    abort(403)
+@app.route('/404')
+def abort404():
+    abort(404)
+@app.route('/500')
+def abort500():
+    abort(500)
+
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(500)
+def error_handler(error):
+    msg = 'Error: {code}\n'.format(code=error.code)
+    return msg, error.code
+@app.errorhandler(404)
+def page_not_found(error):
+    Dict={}
+    Dict["message1"]="エラー"
+    Dict["message2"]=["ページがみつかりません"]
+    html='message.html'
+    return render_template(html,Dict=Dict), 404
 
 
 if __name__ == "__main__":
